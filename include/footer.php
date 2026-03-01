2356f12a17a131837f887bf8f5d769fcd6c50a68
MrFish486 <laszlodeanpav@gmail.com>
2026-02-28 18:33:39 -0800

    Footer enlarge

--- include/footer.php
+++ include/footer.php
@@ -1,1 +1,1 @@
-<img class="footer" src="/assets/noai.png" title="That's right! We wrote this application WITHOUT any form of AI at all."></img>
+<img class="footer" src="/assets/noai.png" title="That's right! We wrote this application WITHOUT any form of AI at all." height="48px" width="96px"></img>
